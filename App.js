73450c12074eb5ec198a48d1984d23e211381fa7
Sathish Kumar M <136421178+SathishKumarM-developer@users.noreply.github.com>
2023-08-28 13:19:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import Fetch from './fetch'
 import Weathers from './Weatherreport';
-//import Apps from './Weathereg1';
-
 
 function App() {
   return(<>
-    <Fetch></Fetch>
     <Weathers></Weathers>
     </>
   )
