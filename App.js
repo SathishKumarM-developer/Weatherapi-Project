b97a7de75ccbdef638db37d112216bd56dd9baec
Sathish Kumar M <136421178+SathishKumarM-developer@users.noreply.github.com>
2023-08-28 13:15:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Fetch from './fetch'
+import Weathers from './Weatherreport';
+//import Apps from './Weathereg1';
+
+
+function App() {
+  return(<>
+    <Fetch></Fetch>
+    <Weathers></Weathers>
+    </>
+  )
+}
+
+export default App;
---
+++ Weatherreport.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function Weathers() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  //const url = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+ 
+ const searchLocation = (search) => 
+  { 
+    {axios.get('https://api.openweathermap.org/data/2.5/weather?units=metric&appid=a7c4e2f81d238476de5016691511ebde&q='+search)
+    .then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    }) 
+    setLocation('')}
+  }
+
+  const handlesubmit=(event)=>{
+        event.preventDefault();
+        searchLocation(location)
+        
+
+  }
+
+ return(<>
+ {console.log(data)}
+  <div className="app">
+    <div className='search'>
+    <form onSubmit={handlesubmit}>
+    <input 
+    value={location} 
+    onChange={event => setLocation(event.target.value)}
+    type='text' placeholder='Enter the Location'></input> </form>   
+    </div>  
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}/>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null }
+          </div>
+        </div>
+        
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {console.log(data.main.feels_like)}
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+          
+        
+
+
+
+      </div>
+   
+      </div>
+  
+  
+  </>)
+
+
+}
+
+
+
+export default Weathers
